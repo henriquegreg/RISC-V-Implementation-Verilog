7acccfd36b7f12ffdc6953fa8715df5e34a1382e
Lucas Bertan <bertan1707@gmail.com>
2022-11-28 00:44:39 -0300

    Update ALU_Control_structural.v

--- ALU_Control/ALU_Control_structural.v
+++ ALU_Control/ALU_Control_structural.v
@@ -4,8 +4,11 @@ module ALU_Control (
     input [3:0] funct,
     output [3:0] operation
 );
+    wire A;
+
     and (operation[3], ALUOp1, ~ALUOp1);
-    and (operation[2], ALUOp1, funct[3]);
-    or (operation[1], ~ALUOp1, ~funct[1]);
+    and (A, ALUOp0, ~ALUOp1);
+    or (operation[2], A, funct[3]);
+    or (operation[1], ALUOp0, ~ALUOp1, ~funct[1]);
     and (operation[0], ALUOp1, funct[2], ~funct[0]);
 endmodule
