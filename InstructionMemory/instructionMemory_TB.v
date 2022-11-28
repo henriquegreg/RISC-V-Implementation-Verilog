e9d358219b7221990bc6bedcedfcaee9787bf67b
Lucas Bertan <bertan1707@gmail.com>
2022-11-28 01:46:48 -0300

    Update instructionMemory_TB.v

--- InstructionMemory/instructionMemory_TB.v
+++ InstructionMemory/instructionMemory_TB.v
@@ -1,5 +1,11 @@
 `timescale 1ns/100ps
 
+/* 
+    Testes feitos antes das condições iniciais do EP.
+    Foi verificada a capacidade da instruction memory de retornar o valor adequado
+    dependendo do valor do PC. Muito parecido com o que foi feito na Memoria de Dados.
+*/
+
 module testbench ();
     reg [31:0] PC;
     wire [31:0] instruction;
@@ -16,7 +22,10 @@ module testbench ();
     endtask
 
     initial begin
-        
+        /* Testa os resultados em uma Memoria de Instrucoes baseada em uma memoria
+           hipotetica com posicao 0 valendo 32'b01000000000000000000000000110011,
+           1 valendo 32'b00000000000000000010000010000011 e 2 valendo
+           32'b00000000000000000000000001100011. O restante e nula */
         PC = 32'd0;
         #10
         Check(32'b01000000000000000000000000110011);
@@ -32,7 +41,7 @@ module testbench ();
         Check(32'b00000000000000000000000001100011);
         #10
 
-        // nao precisa testar tudo, demora demais, depois muda para as instrucoes do ep
+        // nao precisa testar tudo, demora demais, ja foram verificadas varias possibilidades
         for (j = 32'd3; j < 32'b00000000000000000000000011111111; j = j + 32'd1) begin
             PC = j;
             #10
