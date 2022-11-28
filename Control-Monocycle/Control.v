9fe5c0a9b78b9d9399077b85c1e3566dbc8cf6c4
Lucas Bertan <bertan1707@gmail.com>
2022-11-28 01:27:27 -0300

    Add files via upload

--- Control-Monocycle/Control.v
+++ Control-Monocycle/Control.v
@@ -18,13 +18,9 @@ module ControlUnit (
     wire D;
 
     wire [6:0] OpNegated = ~Op;
-    // 0
     and (A, OpNegated[6],Op[5], Op[4], OpNegated[3], OpNegated[2], Op[1], Op[0]);
-    // 1
     and (B, OpNegated[6], OpNegated[5], OpNegated[4], OpNegated[3], OpNegated[2], Op[1], Op[0]);
-    //0
     and (C, OpNegated[6], Op[5], OpNegated[4], OpNegated[3], OpNegated[2], Op[1], Op[0]);
-    //0
     and (D, Op[6], Op[5], OpNegated[4], OpNegated[3], OpNegated[2], Op[1], Op[0]);
 
     assign RegDst = A;
