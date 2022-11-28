f3821354ddd1d377103f9c2252f88ef8699d1127
Lucas Bertan <bertan1707@gmail.com>
2022-11-28 03:24:49 -0300

    Add files via upload

---
+++ DataMemory/generate_datamem.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int numDeTentativas, quantidadeDeTentativas, tentativaAtual, i;
+    printf("module data_memory(input clk,\n input mem_read,\n input mem_write,\n input [31:0] endereco,\n input [31:0] write_data,\n output reg [31:0] read_data\n );\nreg [4:0] endereco_atual;\nreg [31:0] Memory [31:0];\ninteger i;\n // zera a memória inteira e adiciona no final\ninitial begin\n  /* quantidade de tentativas */\n");
+    scanf("%d", &numDeTentativas);
+    printf("Memory[1] = 32'd%d;\n", numDeTentativas);
+
+    for (i = 0; i < numDeTentativas; i++) {
+        scanf("%d", &tentativaAtual);
+        printf("Memory[%d] = 32'd%d;\n", i+2, tentativaAtual);
+    }
+    printf("Memory[12] = 32'd1;\nend\n//  assincrono\nalways @(endereco) begin\n  endereco_atual = endereco[4:0];\n  if (mem_read == 1) begin\n  read_data = Memory[endereco_atual];\n  end\nend\n// sincrono\nalways @(posedge clk) begin\n  endereco_atual = endereco[4:0];\n  if (mem_write == 1) begin\nMemory[endereco_atual] = write_data;\n  end\nend\n endmodule\n");
+    return 0;
+}
